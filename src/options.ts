aad779f6fe095aca847b70b8b532788144a074ce
wangjiwei <wangjiwei@youzan.com>
2020-05-14 18:06:20 +0800

    fix: pick options from program

--- src/options.ts
+++ src/options.ts
@@ -2,24 +2,19 @@ import { ZentVersion } from './zent';
 import { getConfig } from './config';
 import { program } from './program';
 
+const DefaultOptions: IOptions = {
+  target: ZentVersion,
+  quote: 'auto',
+  silent: false,
+  output: false,
+  color: 'javascript',
+  force: false,
+};
+
 export function getOptions(): IOptions {
-  const options: IOptions = {
-    target: ZentVersion,
-    quote: 'auto',
-    silent: false,
-    output: false,
-    color: 'javascript',
-    force: false,
-  };
+  const options: IOptions = { ...DefaultOptions };
   const config = getConfig();
-  Object.assign(options, config?.options, {
-    target: Number(program.target || ZentVersion),
-    silent: program.silent ?? false,
-    quote: program.quote ?? 'auto',
-    output: program.output ?? false,
-    color: program.color ?? 'typescript',
-    force: program.force ?? false,
-  });
+  Object.assign(options, config?.options, pickOptions());
   return options;
 }
 
@@ -30,4 +25,15 @@ export interface IOptions {
   output: boolean;
   color: string;
   force: boolean;
+}
+
+function pickOptions() {
+  const keys = Object.keys(DefaultOptions);
+  const options: Partial<IOptions> = {};
+  for (const key of keys) {
+    if (program[key] !== undefined) {
+      (options as Record<string, unknown>)[key] = program[key];
+    }
+  }
+  return options;
 }
